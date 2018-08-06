4cdca1aaafdf84321dedd6d683db0b5c412772d0
Oscar Sura <surao@stanford.edu>
2018-08-05 19:08:13 -0500

    Verified integrity of generating algorithm, adding output feature.

--- generator.py
+++ generator.py
@@ -1,14 +1,17 @@
 import sys
 from time import sleep
 from random import randint
+from os import system
 
 meta = open("meta", "r")
 meta.readline()
 output_line = meta.readline().strip()
 clues_line = meta.readline().strip()
 num_clues = int(clues_line)
-output = open(output_line, "w")
+output = open(output_line, "w+")
 meta.close()
+
+status_bars = 27
 
 grid_array = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -21,13 +24,33 @@ grid_array = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0]]
  
 def clear():
-    sys.stdout.write("\033[2J")
+    _ = system('clear')
+    #sys.stdout.write("\033[2J")
 
 def hide_cursor():
     sys.stdout.write("\033[?25l")
 
 def show_cursor():
     sys.stdout.write("\033[?25h")
+
+def print_status(gd):
+    print("")
+    full_count = 0
+    for r in range(9):
+        for c in range(9):
+            if gd[r][c] not in [0]:
+                full_count = full_count+1
+    pbar_num = (full_count+0.00)/num_clues
+    pbar = int(pbar_num*status_bars)
+
+    bar = "["
+    for i in range(pbar):
+        bar += unichr(0x2588)
+    for i in range(status_bars - pbar):
+        bar += " "
+    bar += "] "
+    bar += str(full_count) + "/" + str(num_clues)
+    print(bar)
 
 def print_grid(gd):
     clear()
@@ -36,7 +59,7 @@ def print_grid(gd):
         for char in array:
             sys.stdout.write(str(char))
         sys.stdout.write('\n')
-    #print_status(gd)
+    print_status(gd)
 
 def used_in_row(grid_arr, row, num):
     for col in range(9):
@@ -90,14 +113,36 @@ def generate_clue(grid_arr):
     while not valid_move(grid_arr, row, col, num):
         num = random_num()
     grid_arr[row][col] = num
+    print_grid(grid_arr)
+    sleep(.100)
 
 def generate_grid():
     for i in range(num_clues):
         generate_clue(grid_array)
 
+def write_grid(grid_arr):
+    array_count = 0
+    for array in grid_arr:
+        if array_count == 3:
+            output.write('\n')
+            array_count = 0
+
+        int_count = 0
+        for num in array:
+            if int_count == 3:
+                output.write(' ')
+                int_count = 0
+            char = num
+            if num == 0:
+                char = '_'
+            output.write(str(char))
+            int_count = int_count+1
+        output.write('\n')
+        array_count = array_count+1
+
 hide_cursor()
 generate_grid()
-print_grid(grid_array)
+write_grid(grid_array)
 show_cursor()
 
 output.close()
